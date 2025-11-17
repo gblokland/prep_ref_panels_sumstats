d167dd2135b0a3fef647d5bcd3f3756bc4d70420
gblokland <gblokland@users.noreply.github.com>
2025-11-17 14:04:32 +0100

    Update harmonizing_gwas_sumstats.sh

--- harmonizing_gwas_sumstats.sh
+++ harmonizing_gwas_sumstats.sh
@@ -37,21 +37,6 @@ wget https://precimed.s3-eu-west-1.amazonaws.com/python_convert/ref_1kG_phase3_E
 tar xzf ref_1kG_phase3_EUR.tar.gz
 
 
-#CRP - https://doi.org/10.1038/s41467-022-29650-5 
-mkdir -p ~/sumstats/Biomarkers/CRP && cd ~/sumstats/Biomarkers/CRP/
-wget -r -np -R "index.html*" -e robots=off --cut-dirs=8 https://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90029001-GCST90030000/GCST90029070/harmonised/
-
-#Lymphocyte count GWAS: PMID 27863252 x adaptive immune response - https://doi.org/10.1016/j.cell.2016.10.042
-mkdir -p ~/sumstats/Biomarkers/lymphocyte && cd ~/sumstats/Biomarkers/lymphocyte/
-wget -r -np -R "index.html*" -e robots=off --cut-dirs=8 https://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST004001-GCST005000/GCST004632/harmonised/
-
-#Granulocyte count GWAS: PMID 27863252 x innate immune response - https://doi.org/10.1016/j.cell.2016.10.042 
-mkdir -p ~/sumstats/Biomarkers/granulocyte && cd ~/sumstats/Biomarkers/granulocyte/
-wget -r -np -R "index.html*" -e robots=off --cut-dirs=8 https://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST004001-GCST005000/GCST004614/harmonised/
-
-#Granulocytes are a type of white blood cell that has small granules inside them. These granules contain proteins. 
-#The specific types of granulocytes are neutrophils, eosinophils, and basophils.
-
 
 ##################################################################################################
 # Run harmonization
