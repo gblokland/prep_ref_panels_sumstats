77b677563e0f8b9c396c6fc17bf54371cf16f671
gblokland <gblokland@users.noreply.github.com>
2026-02-05 20:51:27 +0100

    Update prep_1KGPref_for_prs_pipeline.sh

--- prep_1KGPref_for_prs_pipeline.sh
+++ prep_1KGPref_for_prs_pipeline.sh
@@ -31,7 +31,7 @@ grep 'AMR' ~/ref_panels/1KGPref/integrated_call_samples_v3.20130502.ALL.panel | 
 
 #Merge ALL.chr${i}.phase3_v5.shapeit2_mvncall_integrated.noSingleton.genotypes.vcf.gz into merged_whole_genome.vcf.gz
 cd ~/ref_panels/1KGPref/
-~/code/vcf_concat_chr.sh
+~/code/prs_pipeline/vcf_concat_chr.sh
 
 #Generate EUR_frequencies.frq
 ~/opt/vcftools --gzvcf ~/ref_panels/1KGPref/merged_whole_genome.vcf.gz --freq \
@@ -46,34 +46,35 @@ cd ~/ref_panels/1KGPref/
 --keep ~/ref_panels/1KGPref/AMR_samples.txt --out ~/ref_panels/1KGPref/AMR_frequencies
 
 cat ~/ref_panels/1KGPref/EUR_frequencies.frq | sed 's/{ALLELE:FREQ}/{ALLELE:FREQ}.1\t{ALLELE:FREQ}.2/g' > ~/ref_panels/1KGPref/EUR_frequencies.frq.edit
-python ~/opt/split_allele_freq.py ~/ref_panels/1KGPref/EUR_frequencies.frq.edit ~/ref_panels/1KGPref/EUR_frequencies.frq.tsv
+python ~/code/prs_pipeline/split_allele_freq.py ~/ref_panels/1KGPref/EUR_frequencies.frq.edit ~/ref_panels/1KGPref/EUR_frequencies.frq.tsv
 
 #Extract SNP rs ids and CHROM and POS from vcf file and write to tsv file
-python ~/opt/extract_vcf_info.py ~/ref_panels/1KGPref/merged_whole_genome.vcf.gz ~/ref_panels/1KGPref/merged_whole_genome_snps.tsv
+python ~/code/prs_pipeline/extract_vcf_info.py ~/ref_panels/1KGPref/merged_whole_genome.vcf.gz ~/ref_panels/1KGPref/merged_whole_genome_snps.tsv
 
-##Merge SNP info with frq file
-#python ~/opt/merge_and_sort_files.py ~/ref_panels/1KGPref/merged_whole_genome_snps.tsv \
-#~/ref_panels/1KGPref/EUR_frequencies.frq.tsv \
-#~/ref_panels/1KGPref/EUR_frequencies.tsv
+#Merge SNP info with frq file
+python ~/code/prs_pipeline/merge_and_sort_files.py ~/ref_panels/1KGPref/merged_whole_genome_snps.tsv ~/ref_panels/1KGPref/EUR_frequencies.frq.tsv \
+~/ref_panels/1KGPref/EUR_frequencies.tsv
 
 cd ~/ref_panels/
-wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/snp147Common.txt.gz
-gunzip snp147Common.txt.gz
-###cut -f2-5 snp147Common.txt | awk '{gsub("chr", "", $2); print $4"\t$1"\t"$2"\t"($3+1)}' > rsid_chr_bp_map.tsv
-cut -f2-5 snp147Common.txt | awk 'BEGIN {print "SNP\tCHR\tBP\tBP_END"} {gsub("chr", "", $1); print $4"\t"$1"\t"$2"\t"$3}' > rsid_chr_bp_map.tsv
+mkdir -p ucsc_hg19 && cd ucsc_hg19
+wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/snp151Common.txt.gz
+gunzip snp151Common.txt.gz
+###cut -f2-5 snp151Common.txt | awk '{gsub("chr", "", $2); print $4"\t$1"\t"$2"\t"($3+1)}' > rsid_chr_bp_map.tsv
+cut -f2-5 snp151Common.txt | awk 'BEGIN {print "SNP\tCHR\tBP\tBP_END"} {gsub("chr", "", $1); print $4"\t"$1"\t"$2"\t"$3}' > rsid_chr_bp_map.tsv
 
 ##################################################################################################
 #Run format conversion
 cd ~/ref_panels
 mkdir -p ensembl_grch37 && cd ensembl_grch37
 
-for chr in {1..22} X Y MT; do
-wget ftp://ftp.ensembl.org/pub/grch37/release-105/variation/vcf/homo_sapiens/homo_sapiens-chr${chr}.vcf.gz
-wget ftp://ftp.ensembl.org/pub/grch37/release-105/variation/vcf/homo_sapiens/homo_sapiens-chr${chr}.vcf.gz.csi
-tabix -p vcf homo_sapiens-chr${chr}.vcf.gz #make *.tbi file
-done
-
-wget -r -nH --cut-dirs=5 -A "homo_sapiens-chr*.vcf.gz,homo_sapiens-chr*.vcf.gz.tbi" ftp://ftp.ensembl.org/pub/grch37/current/variation/vcf/homo_sapiens/
+if [ ! -f ~/ref_panels/ensembl_grch37/homo_sapiens-chr22.vcf.gz.csi ]; then
+    for chr in {1..22} X Y MT; do
+        wget ftp://ftp.ensembl.org/pub/grch37/release-105/variation/vcf/homo_sapiens/homo_sapiens-chr${chr}.vcf.gz
+        wget ftp://ftp.ensembl.org/pub/grch37/release-105/variation/vcf/homo_sapiens/homo_sapiens-chr${chr}.vcf.gz.csi
+        tabix -p vcf homo_sapiens-chr${chr}.vcf.gz #make *.tbi file
+    done
+    wget -r -nH --cut-dirs=5 -A "homo_sapiens-chr*.vcf.gz,homo_sapiens-chr*.vcf.gz.tbi" ftp://ftp.ensembl.org/pub/grch37/current/variation/vcf/homo_sapiens/
+fi
 
 cd homo_sapiens
 
