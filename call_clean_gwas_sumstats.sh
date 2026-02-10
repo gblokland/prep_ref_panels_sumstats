ada282ebfaef532ee67170d216d3e56198a3fe9d
gblokland <gblokland@users.noreply.github.com>
2026-02-10 12:45:08 +0100

    Update call_clean_gwas_sumstats.sh

--- call_clean_gwas_sumstats.sh
+++ call_clean_gwas_sumstats.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 python clean_gwas_sumstats.py \
-    --input gwas_raw.txt.gz \
-    --reference_maf 1000G_EUR_ref.tsv \
-    --liftover_chain hg19ToHg38.over.chain.gz \
-    --output gwas_clean.tsv \
+    --input /root/persistent/sumstats/phenotype_category/gwas_raw.txt.gz \
+    --reference_maf /root/persistent/ref_panels/1KGPref/1000G_EUR_ref.tsv \
+    --liftover_chain /root/persistent/ref_panels/hg19ToHg38.over.chain.gz \
+    --output /root/persistent/sumstats_harmonized/gwas_clean.tsv \
     --gzip_output \
-    --logfile gwas_clean.log
+    --logfile /root/persistent/sumstats_harmonized/gwas_clean.log
 
 python clean_gwas_summstats.py \
-    --input PGC_SCZ_sumstats.tar.gz \
-    --reference_maf 1000G_EUR_ref.tsv \
-    --output PGC_SCZ_clean.tsv \
+    --input /root/persistent/sumstats/phenotype_category/PGC_SCZ_sumstats.tar.gz \
+    --reference_maf /root/persistent/ref_panels/1KGPref/1000G_EUR_ref.tsv \
+    --output /root/persistent/sumstats_harmonized/PGC_SCZ_clean.tsv \
     --gzip_output \
-    --logfile PGC_SCZ_clean.log
+    --logfile /root/persistent/sumstats_harmonized/PGC_SCZ_clean.log
